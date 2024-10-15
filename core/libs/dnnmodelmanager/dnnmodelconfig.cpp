37fdbf60107a45cbfa1ea30d9ea3a955063f9a8e
Gilles Caulier <caulier.gilles@gmail.com>
2024-10-15 04:21:22 +0000

    code review and polish

--- core/libs/dnnmodelmanager/dnnmodelconfig.cpp
+++ core/libs/dnnmodelmanager/dnnmodelconfig.cpp
@@ -12,6 +12,8 @@
  *
  * ============================================================ */
 
+#include "dnnmodelconfig.h"
+
 // Qt includes
 
 #include <QStandardPaths>
@@ -19,78 +21,72 @@
 // Local includes
 
 #include "digikam_debug.h"
-#include "dnnmodelbase.h"
-#include "dnnmodelconfig.h"
 
 namespace Digikam
 {
 
-
-// DNNModelBase::DNNModelBase()
 DNNModelConfig::DNNModelConfig(
-                 const QString&                 _displayName,
-                 const QString&                 _fileName,
-                 const DNNModelUsageList&       _usage,
-                 const QVersionNumber&          _minVersion,
-                 const QString&                 _downloadPath,
-                 const QString&                 _sha256,
-                 const qint64&                  _fileSize,
-                 int                            _minUsableThreshold,
-                 int                            _maxUsableThreshold,
-                 DNNLoaderType                  _loaderType,
-                 const QString&                 _configName,
-                 const cv::Scalar&              _meanValToSubtract,
-                 int                            _imageSize
-                )
-            : DNNModelBase(
-                            _displayName,
-                            _fileName,
-                            _usage,
-                            _minVersion,
-                            _downloadPath,
-                            _sha256,
-                            _fileSize,
-                            _minUsableThreshold,
-                            _maxUsableThreshold,
-                            _loaderType,
-                            _configName,
-                            _meanValToSubtract,
-                            _imageSize
-                          )
-{
-
-}
-
-DNNModelConfig::~DNNModelConfig()
+                               const QString&                 _displayName,
+                               const QString&                 _fileName,
+                               const DNNModelUsageList&       _usage,
+                               const QVersionNumber&          _minVersion,
+                               const QString&                 _downloadPath,
+                               const QString&                 _sha256,
+                               const qint64&                  _fileSize,
+                               int                            _minUsableThreshold,
+                               int                            _maxUsableThreshold,
+                               DNNLoaderType                  _loaderType,
+                               const QString&                 _configName,
+                               const cv::Scalar&              _meanValToSubtract,
+                               int                            _imageSize
+                              )
+    : DNNModelBase(
+                   _displayName,
+                   _fileName,
+                   _usage,
+                   _minVersion,
+                   _downloadPath,
+                   _sha256,
+                   _fileSize,
+                   _minUsableThreshold,
+                   _maxUsableThreshold,
+                   _loaderType,
+                   _configName,
+                   _meanValToSubtract,
+                   _imageSize
+                  )
 {
 }
 
-// const QString DNNModelConfig::getConfig()
-// {
-//     if (!modelLoaded)
-//     {
-//         loadModel();
-//         QString appPath = QStandardPaths::locate(QStandardPaths::GenericDataLocation,
-//                                                 QLatin1String("digikam/") + (downloadPath.split(QLatin1String("/"))[0]),
-//                                                 QStandardPaths::LocateDirectory);
+/*
+const QString DNNModelConfig::getConfig()
+{
+    if (!modelLoaded)
+    {
+        loadModel();
+        QString appPath = QStandardPaths::locate(QStandardPaths::GenericDataLocation,
+                                                 QLatin1String("digikam/") + (downloadPath.split(QLatin1String("/"))[0]),
+                                                 QStandardPaths::LocateDirectory);
 
-//         QString model = appPath + QLatin1Char('/') + fileName;
-//         return model;
-//     }
-    
-//     return QLatin1String();
-// }
+        QString model = appPath + QLatin1Char('/') + fileName;
+
+        return model;
+    }
+
+    return QLatin1String();
+}
+*/
 
 bool DNNModelConfig::loadModel()
 {
     if (checkFilename())
-    {    // do nothing.  Config don't have a cv::dnn::Net pointer
+    {
+        // do nothing.  Config don't have a cv::dnn::Net pointer
+
         return modelLoaded = true;  // set modelLoaded and return true
     }
+
     return false;
 }
 
-
 } // namespace Digikam
-
-// #include "moc_DNNModelBase.cpp"
--- core/libs/dnnmodelmanager/dnnmodelconfig.h
+++ core/libs/dnnmodelmanager/dnnmodelconfig.h
@@ -22,6 +22,7 @@
 
 #include "digikam_opencv.h"
 #include "digikam_export.h"
+#include "dnnmodelbase.h"
 #include "dnnmodelmanager.h"
 
 namespace Digikam
@@ -31,37 +32,43 @@ class DIGIKAM_EXPORT DNNModelConfig: public DNNModelBase
 {
 
 public:
-    // ---------- public methods ----------
-    DNNModelConfig(
-                 const QString&                 _displayName,
-                 const QString&                 _fileName,
-                 const DNNModelUsageList&       _usage,
-                 const QVersionNumber&          _minVersion,
-                 const QString&                 _downloadPath,
-                 const QString&                 _sha256,
-                 const qint64&                  _fileSize,
-                 int                            _minUsableThreshold,
-                 int                            _maxUsableThreshold,
-                 DNNLoaderType                  _loaderType,
-                 const QString&                 _configName,
-                 const cv::Scalar&              _meanValToSubtract,
-                 int                            _imageSize
-                );   
-    virtual ~DNNModelConfig()  override;
 
+    // ---------- public methods ----------
+
+    DNNModelConfig(
+                   const QString&                 _displayName,
+                   const QString&                 _fileName,
+                   const DNNModelUsageList&       _usage,
+                   const QVersionNumber&          _minVersion,
+                   const QString&                 _downloadPath,
+                   const QString&                 _sha256,
+                   const qint64&                  _fileSize,
+                   int                            _minUsableThreshold,
+                   int                            _maxUsableThreshold,
+                   DNNLoaderType                  _loaderType,
+                   const QString&                 _configName,
+                   const cv::Scalar&              _meanValToSubtract,
+                   int                            _imageSize
+                  );
+    virtual ~DNNModelConfig() override = default;
+
+/*
     // input: uiThreshold is the slider value from the UI
     // return: float threshold to be used by processing (FaceDetector, FaceRecognizer, etc...)
-    // float processingThreshold(int uiThreshold);
+    float processingThreshold(int uiThreshold);
+*/
 
 public:
-    // ---------- public members ----------
-    // const QString      getConfig();
 
+    // ---------- public members ----------
+/*
+    const QString      getConfig();
+*/
 private:
+
     DNNModelConfig()               = delete;
 
-    virtual bool loadModel() override;                  // must be overridden in child class
-
+    virtual bool loadModel() override;                  ///< must be overridden in child class
 };
 
 } // namespace Digikam
