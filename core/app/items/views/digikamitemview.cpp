d4a38093193b9b4c7746aa5928bc2680122ee2ca
Maik Qualmann <metzpinguin@gmail.com>
2024-11-14 19:40:55 +0100

    Revert "fix confirmation of different faces" Restore old face confirmation behavior. BUGS: 496131 FIXED-IN: 8.5.0

--- core/app/items/views/digikamitemview.cpp
+++ core/app/items/views/digikamitemview.cpp
@@ -436,17 +436,7 @@ void DigikamItemView::confirmFaces(const QList<QModelIndex>& indexes, int tagId)
 
     for (int i = 0 ; i < infos.size() ; ++i)
     {
-        int newTagId = tagId;
-
-        if ((infos.size() > 1)                            &&
-            (faces[i].tagId() != newTagId)                &&
-            !FaceTags::isSystemPersonTagId(faces[i].tagId())
-           )
-        {
-            newTagId = faces[i].tagId();
-        }
-
-        d->editPipeline.confirm(infos[i], faces[i], newTagId);
+        d->editPipeline.confirm(infos[i], faces[i], tagId);
     }
 
     imageAlbumModel()->removeIndexes(sourceIndexes);
