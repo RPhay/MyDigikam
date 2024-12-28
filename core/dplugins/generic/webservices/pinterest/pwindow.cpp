d9f5062ef6321c53e23e34191fb586f2dccecbf8
Gilles Caulier <caulier.gilles@gmail.com>
2024-12-28 11:53:11 +0000

    krazy++: add i18n context

--- core/dplugins/generic/webservices/pinterest/pwindow.cpp
+++ core/dplugins/generic/webservices/pinterest/pwindow.cpp
@@ -48,6 +48,8 @@ public:
 
     Private() = default;
 
+public:
+
     unsigned int  imagesCount       = 0;
     unsigned int  imagesTotal       = 0;
 
@@ -237,7 +239,7 @@ void PWindow::slotStartTransfer()
 
     if (d->widget->imagesList()->imageUrls().isEmpty())
     {
-        QMessageBox::critical(this, i18nc("@title:window", "Error"),
+        QMessageBox::critical(this, i18nc("@title: window start transfert", "Error"),
                               i18n("No image selected. Please select which images should be uploaded."));
         return;
     }
@@ -245,12 +247,12 @@ void PWindow::slotStartTransfer()
     if (!(d->talker->authenticated()))
     {
         QPointer<QMessageBox> warn = new QMessageBox(QMessageBox::Warning,
-            i18nc("@title:window", "Warning"),
+            i18nc("@title: window start transfert", "Warning"),
             i18n("Authentication failed. Click \"Continue\" to authenticate."),
             QMessageBox::Yes | QMessageBox::No);
 
-        warn->button(QMessageBox::Yes)->setText(i18nc("@action:button", "Continue"));
-        warn->button(QMessageBox::No)->setText(i18nc("@action:button", "Cancel"));
+        warn->button(QMessageBox::Yes)->setText(i18nc("@action: button start transfert", "Continue"));
+        warn->button(QMessageBox::No)->setText(i18nc("@action: button start transfert", "Cancel"));
 
         if (warn->exec() == QMessageBox::Yes)
         {
