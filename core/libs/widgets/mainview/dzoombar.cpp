6b1a72678235f1ec553bd052b946c40ea693d813
Gilles Caulier <caulier.gilles@gmail.com>
2024-12-28 09:09:22 +0000

    krazy++: no need extra QString as argument

--- core/libs/widgets/mainview/dzoombar.cpp
+++ core/libs/widgets/mainview/dzoombar.cpp
@@ -207,7 +207,7 @@ void DZoomBar::setZoom(double zoom, double zmin, double zmax)
     d->zoomSlider->setValue(size);
     d->zoomSlider->blockSignals(false);
 
-    QString ztxt = i18nc("%1 is the zoom percent value, % is the percent sign", "%1%", QString::number(lround(zoom*100.0)));
+    QString ztxt = i18nc("%1 is the zoom percent value, % is the percent sign", "%1%", lround(zoom*100.0));
     d->zoomCombo->blockSignals(true);
     d->zoomCombo->setCurrentIndex(-1);
     d->zoomCombo->setEditText(ztxt);
