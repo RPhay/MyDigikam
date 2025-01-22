bbfe2b50e876835ffd7f0d85b2cae94ed7032443
Gilles Caulier <caulier.gilles@gmail.com>
2025-01-22 15:23:44 +0000

    krazy null --

--- core/libs/jpegutils/jpegwin.cpp
+++ core/libs/jpegutils/jpegwin.cpp
@@ -67,9 +67,9 @@ void term_source (j_decompress_ptr /*cinfo*/)
 
 void jpeg_memory_src (j_decompress_ptr cinfo, const JOCTET* buffer, size_t bufsize)
 {
-    digikam_source_mgr* src = NULL;
+    digikam_source_mgr* src = NULL;   //  krazy:exclude=null
 
-    if (cinfo->src == NULL)
+    if (cinfo->src == NULL)           //  krazy:exclude=null
     {
         cinfo->src = (struct jpeg_source_mgr*) (*cinfo->mem->alloc_small) ((j_common_ptr) cinfo,
                         JPOOL_PERMANENT,
