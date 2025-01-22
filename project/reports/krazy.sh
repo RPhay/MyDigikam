4a216c434daaf370a93f2d2bc4cb676a4b8a9f04
Gilles Caulier <caulier.gilles@gmail.com>
2025-01-22 13:04:20 +0100

    enable extra Krazy checkers

--- project/reports/krazy.sh
+++ project/reports/krazy.sh
@@ -81,7 +81,7 @@ krazy2all --export xml \
           --priority all \
           --verbose \
           --check-set kde5,foss \
-          --extra null,contractions \
+          --extra null,contractions,defines,license-reuse,copyright-reuse,qmacros,tipsandthis \
           --exclude qclasses,license \
           --exclude-types qml,python,qdoc,perl \
           --topdir ../../ \
