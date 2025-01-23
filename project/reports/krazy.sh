9fc80dbc62e3dbe1160f451bc56f36586dadaebc
Gilles Caulier <caulier.gilles@gmail.com>
2025-01-23 09:57:10 +0100

    this checker do not pass at end with test codes

--- project/reports/krazy.sh
+++ project/reports/krazy.sh
@@ -81,7 +81,7 @@ krazy2all --export xml \
           --priority all \
           --verbose \
           --check-set kde5,foss \
-          --extra null,contractions,defines,license-reuse,copyright-reuse,qmacros,tipsandthis \
+          --extra null,contractions,defines,license-reuse,copyright-reuse,qmacros \
           --exclude qclasses,license \
           --exclude-types qml,python,qdoc,perl \
           --topdir ../../ \
