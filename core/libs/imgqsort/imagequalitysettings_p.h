4dc1666e193503575b6a092d26af7a9cc022998f
Gilles Caulier <caulier.gilles@gmail.com>
2025-01-27 11:12:07 +0100

    file missing

---
+++ core/libs/imgqsort/imagequalitysettings_p.h
@@ -0,0 +1,95 @@
+/* ============================================================
+ *
+ * This file is a part of digiKam project
+ * https://www.digikam.org
+ *
+ * Date        : 2013-08-19
+ * Description : Image Quality settings widget
+ *
+ * SPDX-FileCopyrightText: 2013-2025 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ * SPDX-FileCopyrightText: 2013-2014 by Gowtham Ashok <gwty93 at gmail dot com>
+ * SPDX-FileCopyrightText: 2021-2022 by Phuoc Khanh Le <phuockhanhnk94 at gmail dot com>
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ *
+ * ============================================================ */
+
+#pragma once
+
+#include "imagequalitysettings.h"
+
+// Qt includes
+
+#include <QCheckBox>
+#include <QRadioButton>
+#include <QButtonGroup>
+#include <QGroupBox>
+#include <QGridLayout>
+#include <QLabel>
+#include <QVBoxLayout>
+#include <QIcon>
+#include <QApplication>
+#include <QStyle>
+#include <QPushButton>
+
+// KDE includes
+
+#include <klocalizedstring.h>
+
+// Local includes
+
+#include "dlayoutbox.h"
+#include "picklabelwidget.h"
+#include "dnuminput.h"
+
+namespace Digikam
+{
+
+class Q_DECL_HIDDEN ImageQualitySettings::Private
+{
+public:
+
+    enum DetectMethod
+    {
+        AESTHETIC = 0,
+        BASICFACTORS
+    };
+
+public:
+
+    Private() = default;
+
+public:
+
+    QWidget*      rulesWidget           = nullptr;
+    QWidget*      basicView             = nullptr;
+
+    QCheckBox*    detectBlur            = nullptr;
+    QCheckBox*    detectNoise           = nullptr;
+    QCheckBox*    detectCompression     = nullptr;
+    QCheckBox*    detectExposure        = nullptr;
+
+    QButtonGroup* detectButtonGroup     = nullptr;
+    QRadioButton* detectAesthetic       = nullptr;
+    QRadioButton* detectBasicFactors    = nullptr;
+
+    QCheckBox*    setRejected           = nullptr;
+    QCheckBox*    setPending            = nullptr;
+    QCheckBox*    setAccepted           = nullptr;
+
+    QLabel*       lbl2                  = nullptr;
+    QLabel*       lbl3                  = nullptr;
+    QLabel*       lbl4                  = nullptr;
+    QLabel*       lbl5                  = nullptr;
+    QLabel*       lbl6                  = nullptr;
+    QLabel*       lbl7                  = nullptr;
+
+    DIntNumInput* setRejectedThreshold  = nullptr;
+    DIntNumInput* setPendingThreshold   = nullptr;
+    DIntNumInput* setAcceptedThreshold  = nullptr;
+    DIntNumInput* setBlurWeight         = nullptr;
+    DIntNumInput* setNoiseWeight        = nullptr;
+    DIntNumInput* setCompressionWeight  = nullptr;
+};
+
+} // namespace Digikam
