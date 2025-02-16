6171a86441882040cff289f378253daf3f66645e
Maik Qualmann <metzpinguin@gmail.com>
2025-02-16 19:44:14 +0100

    delete ExifToolThread

--- core/libs/metadataengine/exiftool/exiftoolthread.cpp
+++ core/libs/metadataengine/exiftool/exiftoolthread.cpp
@@ -31,14 +31,10 @@ ExifToolThread::ExifToolThread(QObject* const parent)
         {
             quit();
             wait();
+
+            delete this;
         }
     );
-}
-
-ExifToolThread::~ExifToolThread()
-{
-    quit();
-    wait();
 }
 
 void ExifToolThread::run()
