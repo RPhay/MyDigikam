7ea606a3e499a857c9e1fdbbafeea93f380d625e
Michael Miller <michael_miller@msn.com>
2025-03-23 23:08:02 -0400

    update versions

--- project/bundles/homebrew/config.sh
+++ project/bundles/homebrew/config.sh
@@ -141,15 +141,15 @@ else
 
     # KDE Plasma version.
     # See official release here: https://download.kde.org/stable/plasma/
-    DK_KP_VERSION="v6.2.1"
+    DK_KP_VERSION="v6.2.5"
 
     # KDE Application version.
     # See official release here: https://download.kde.org/stable/release-service/
-    DK_KA_VERSION="v24.08.2"
+    DK_KA_VERSION="v24.12.1"
 
     # KDE KF6 frameworks version.
     # See official release here: https://download.kde.org/stable/frameworks/
-    DK_KDE_VERSION="v6.7.0"
+    DK_KDE_VERSION="v6.10.0"
 
     DK_UPLOADDIR="/srv/archives/files/digikam/unstable"
 
