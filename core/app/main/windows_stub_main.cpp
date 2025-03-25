52bf0baedb0042d0351b224357145c1c5f51dbe0
Gilles Caulier <caulier.gilles@gmail.com>
2025-03-25 09:14:28 +0100

    add comments

--- core/app/main/windows_stub_main.cpp
+++ core/app/main/windows_stub_main.cpp
@@ -5,6 +5,9 @@
  *
  * Date        : 2002-07-28
  * Description : main program from digiKam (stub for Windows binary)
+ *               The main implementation is built into a DLL to be called by a stub EXE.
+ *               This is a work around "command line is too long" issue on Windows.
+ *               see https://stackoverflow.com/questions/43184251/cmake-command-line-too-long-windows
  *
  * SPDX-FileCopyrightText: 2002-2025 by Gilles Caulier <caulier dot gilles at gmail dot com>
  *
