e4a145f5e50b134c2e3be45c4844f84e60b67954
Gilles Caulier <caulier.gilles@gmail.com>
2025-04-03 11:03:39 +0200

    use new openssl cmake port project from github, more up-to-date and better maintained

---
+++ project/scripts/bootstrap-opensslcmake.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# SPDX-FileCopyrightText: 2008-2025 by Gilles Caulier, <caulier dot gilles at gmail dot com>
+#
+# SPDX-License-Identifier: BSD-3-Clause
+#
+# Copy this script on root folder where are source code
+
+#export VERBOSE=1
+
+# We will work on command line using MinGW compiler
+export MAKEFILES_TYPE='Unix Makefiles'
+
+if [ ! -d "build" ]; then
+    mkdir build
+fi
+
+cd build
+
+export Options='-DCMAKE_POSITION_INDEPENDENT_CODE=TRUE \
+                -DOPENSSL_BUILD_VERBOSE=ON \
+                -DOPENSSL_ENABLE_PARALLEL=ON \
+                -DOPENSSL_INSTALL=OFF \
+                -DOPENSSL_INSTALL_CERT=OFF \
+                -DOPENSSL_TEST=OFF \
+                -DOPENSSL_USE_CCACHE=OFF'
+
+cmake -G "$MAKEFILES_TYPE" . \
+      -DCMAKE_INSTALL_PREFIX=/usr \
+      -Wno-dev \
+      $Options \
+      ..
+
