873356a2df7e6827c0ac27e8a87442831b6a8da2
Gilles Caulier <caulier.gilles@gmail.com>
2025-04-16 08:13:12 +0200

    NVIDIA toolkit dlls are not redistributable and cannot be included in the digiKam Windows installer. These toolkits must be installed separatly by the user on the target computer. Add a release notes section about the AI CUDA support in digiKam with the relevant instructions to turn on NVIDIA GPU / OpenCL support in digiKam. CCBUGS: 501236

--- project/bundles/vcpkg/04-build-installer.sh
+++ project/bundles/vcpkg/04-build-installer.sh
@@ -10,7 +10,6 @@
 #   - Mt from VSCommunity C++ Desktop tools.
 #   - ImageMagick for the run-time dlls.
 #   - Msys2/Rsync CLI tools for Windows.
-#   - Cuda+CudaDnn toolkits from NVIDIA.
 #
 # SPDX-FileCopyrightText: 2015-2025 by Gilles Caulier  <caulier dot gilles at gmail dot com>
 #
@@ -93,30 +92,6 @@ if [ ! -d "$MAGICK_PREFIX" ] ; then
     exit 1
 else
     echo "Check ImageMagick install path passed..."
-fi
-
-# Check for the CUDA toolkits install directory.
-
-CUDASDK_PREFIX="`find "/c/Program Files/NVIDIA GPU Computing Toolkit" -name "cublas64_12.dll" -type f -executable`"
-CUDASDK_PREFIX="`dirname "${CUDASDK_PREFIX}"`"
-echo "$CUDASDK_PREFIX"
-
-if [ ! -d "$CUDASDK_PREFIX" ] ; then
-    echo "CUDA toolkit is not installed"
-    exit 1
-else
-    echo "Check CUDA toolkit install path passed..."
-fi
-
-CUDADNNSDK_PREFIX="`find "/c/Program Files/NVIDIA/CUDNN/v9.8/bin/11.8" -name "cudnn64_9.dll" -type f -executable`"
-CUDADNNSDK_PREFIX="`dirname "${CUDADNNSDK_PREFIX}"`"
-echo "$CUDADNNSDK_PREFIX"
-
-if [ ! -d "$CUDADNNSDK_PREFIX" ] ; then
-    echo "CUDA-DNN toolkit is not installed"
-    exit 1
-else
-    echo "Check CUDA-DNN toolkit install path passed..."
 fi
 
 #################################################################################################
@@ -455,12 +430,6 @@ for ffmpegdll in $FFMPEG_DLL_FILES ; do
     CopyReccursiveDependencies "$DUMP_BIN" "$ffmpegdll" "$BUNDLEDIR/" "$VCPKG_INSTALL_PREFIX/bin"
 
 done
-
-echo -e "\n---------- Copy CUDA toolkits dlls"
-
-cp "$CUDASDK_PREFIX"/cublas64_12.dll                                    $BUNDLEDIR/                           2>/dev/null
-cp "$CUDASDK_PREFIX"/cublasLt64_12.dll                                  $BUNDLEDIR/                           2>/dev/null
-cp "$CUDADNNSDK_PREFIX"/cudnn64_9.dll                                   $BUNDLEDIR/                           2>/dev/null
 
 echo -e "\n---------- Copy redistributable VSCommunity compatibility dlls"
 
