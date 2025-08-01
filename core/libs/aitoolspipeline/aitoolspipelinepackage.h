ec3aebe155f9bda8dea749ff2a3787a31283789a
Gilles Caulier <caulier.gilles@gmail.com>
2025-08-01 04:06:42 +0200

    krazy++

--- core/libs/aitoolspipeline/aitoolspipelinepackage.h
+++ core/libs/aitoolspipeline/aitoolspipelinepackage.h
@@ -6,7 +6,7 @@
  * Date        : 2025-05-02
  * Description : AI tools pipeline package class
  *
- * SPDX-FileCopyrightText : 2024-2025 by Michael Miller <michael underscore miller at msn dot com>
+ * SPDX-FileCopyrightText: 2024-2025 by Michael Miller <michael underscore miller at msn dot com>
  *
  * SPDX-License-Identifier: GPL-2.0-or-later
  *
