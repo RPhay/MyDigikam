6b312fc5bb6ae891253fd49f6177cd5c247ef118
Gilles Caulier <caulier.gilles@gmail.com>
2025-08-04 11:01:31 +0200

    polish

--- project/bundles/3rdparty/ext_qt6/config-qt6-macos.cmake
+++ project/bundles/3rdparty/ext_qt6/config-qt6-macos.cmake
@@ -1,7 +1,7 @@
 ################################################################################
 #
 # Script to build Qt 6 - config for MacOS
-# See option details on configure-help text file
+# See option details on config-help text file
 #
 # Copyright (c) 2015-2025 by Gilles Caulier  <caulier dot gilles at gmail dot com>
 #
