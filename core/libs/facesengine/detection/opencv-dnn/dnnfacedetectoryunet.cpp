89879f4a401840f99303cbd9a11ae8af742b9ee4
Gilles Caulier <caulier.gilles@gmail.com>
2025-08-17 17:51:49 +0200

    fix broken compilation

--- core/libs/facesengine/detection/opencv-dnn/dnnfacedetectoryunet.cpp
+++ core/libs/facesengine/detection/opencv-dnn/dnnfacedetectoryunet.cpp
@@ -218,7 +218,7 @@ cv::UMat DNNFaceDetectorYuNet::callModel(const cv::UMat& inputImage)
 
         catch (const std::string& e)
         {
-            qCWarning(DIGIKAM_FACESENGINE_LOG) << "cv::Exception:" << e.what();
+            qCWarning(DIGIKAM_FACESENGINE_LOG) << "cv::Exception:" << e;
         }
 
         catch (...)
