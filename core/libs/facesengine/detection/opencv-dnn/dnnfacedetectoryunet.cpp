5b132f987d115872749c99f6bd537ed3944480f0
Gilles Caulier <caulier.gilles@gmail.com>
2025-08-18 14:45:12 +0200

    fix Qt5 build

--- core/libs/facesengine/detection/opencv-dnn/dnnfacedetectoryunet.cpp
+++ core/libs/facesengine/detection/opencv-dnn/dnnfacedetectoryunet.cpp
@@ -218,7 +218,7 @@ cv::UMat DNNFaceDetectorYuNet::callModel(const cv::UMat& inputImage)
 
         catch (const std::string& e)
         {
-            qCWarning(DIGIKAM_FACESENGINE_LOG) << "cv::Exception:" << e;
+            qCWarning(DIGIKAM_FACESENGINE_LOG) << "cv::Exception:" << QString::fromStdString(e);
         }
 
         catch (...)
