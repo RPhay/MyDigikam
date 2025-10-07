7f2d3701706cd58547c874f51e6e096f65f1ea49
Gilles Caulier <caulier.gilles@gmail.com>
2025-10-07 08:02:04 +0200

    cleanup

--- core/libs/dimg/filters/lens/lensfuniface.h
+++ core/libs/dimg/filters/lens/lensfuniface.h
@@ -17,9 +17,6 @@
 /*
  * Pragma directives to reduce warnings from Lensfun header files.
  *
- * TODO: lensfun version > 0.3.2 introduce deprecated methods for the future.
- * digiKam Code need to be ported to new API when Lensfun 0.4.0 will be related.
- *
  */
 #if defined(Q_CC_GNU)
 #   pragma GCC diagnostic push
