5cb0c743cadc83459afa3ddd8c74f8a52a0c104a
Gilles Caulier <caulier.gilles@gmail.com>
2025-10-11 14:51:13 +0200

    qt 6.10 has removed QtX11Extra headers. We need to found a solution based on these tips: https://doc.qt.io/qt-6/extras-changes-qt6.html#changes-to-qt-x11-extras

--- core/libs/dimg/filters/icc/iccsettings_p.h
+++ core/libs/dimg/filters/icc/iccsettings_p.h
@@ -32,7 +32,7 @@
 #include <QMutexLocker>
 
 #ifdef HAVE_X11
-#   if QT_VERSION >= QT_VERSION_CHECK(6, 0, 0)
+#   if QT_VERSION >= QT_VERSION_CHECK(6, 0, 0) && QT_VERSION < QT_VERSION_CHECK(6, 10, 0)
 #       include <private/qtx11extras_p.h>
 #   else
 #       include <QX11Info>
