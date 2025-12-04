d739ea1dc81c7df3cb19cc6ffae5cec10aafd4b6
Gilles Caulier <caulier.gilles@gmail.com>
2025-12-04 10:25:30 +0100

    bump versions

--- project/bundles/appimage/config.sh
+++ project/bundles/appimage/config.sh
@@ -85,15 +85,15 @@ else
 
     # KDE KF6 frameworks version.
     # See official release here: https://download.kde.org/stable/frameworks/
-    DK_KDE_VERSION="v6.17.0"
+    DK_KDE_VERSION="v6.20.0"
 
     # KDE Plasma version.
     # See official release here: https://download.kde.org/stable/plasma/
-    DK_KP_VERSION="v6.4.4"
+    DK_KP_VERSION="v6.5.3"
 
     # KDE Application version.
     # See official release here: https://download.kde.org/stable/release-service/
-    DK_KA_VERSION="v25.04.3"
+    DK_KA_VERSION="v25.08.3"
 
     # Absolute path where are downloaded all tarballs to compile.
     DOWNLOAD_DIR="/mnt/data/d"
