dd3f00d8700434c3ade8f717172434ddf564d747
Srirupa Datta <srirupa.sps@gmail.com>
2026-02-06 08:24:30 -0500

    Add bug to NEWS file, add name in headers

--- core/libs/database/coredb/coredb.h
+++ core/libs/database/coredb/coredb.h
@@ -10,7 +10,7 @@
  * SPDX-FileCopyrightText: 2006-2026 by Gilles Caulier <caulier dot gilles at gmail dot com>
  * SPDX-FileCopyrightText: 2006-2012 by Marcel Wiesweg <marcel dot wiesweg at gmx dot de>
  * SPDX-FileCopyrightText: 2012      by Andi Clemens <andi dot clemens at gmail dot com>
- *
+ * SPDX-FileCopyrightText: 2026      by Srirupa Datta <srirupa dot sps at gmail dot com>
  * SPDX-License-Identifier: GPL-2.0-or-later
  *
  * ============================================================ */
