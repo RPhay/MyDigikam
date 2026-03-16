57ad1085ad644e94e77dc57582e387b81d0cd5b8
Gilles Caulier <caulier.gilles@gmail.com>
2026-03-16 05:02:27 +0100

    bump version

--- project/bundles/macports/config.sh
+++ project/bundles/macports/config.sh
@@ -163,7 +163,7 @@ else
 
     # KDE KF6 frameworks version.
     # See official release here: https://download.kde.org/stable/frameworks/
-    DK_KDE_VERSION="v6.23.0"
+    DK_KDE_VERSION="v6.24.0"
 
     # KDE Plasma version.
     # See official release here: https://download.kde.org/stable/plasma/
