ada7a494b22c54d2735e79ecffbfe7dd2e33906a
Michael Miller <michael_miller@msn.com>
2026-03-29 13:25:19 -0400

    Added MetalToolchain to Homebrew

--- project/bundles/homebrew/common.sh
+++ project/bundles/homebrew/common.sh
@@ -70,6 +70,7 @@ fi
 # Adjust the property "MinimumSDKVersion" from /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Info.plist
 /usr/libexec/PlistBuddy -c "Set MinimumSDKVersion $MACOS_MAJOR.$MACOS_MINOR" /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Info.plist
 
+xcodebuild -downloadComponent MetalToolchain
 }
 
 ########################################################################
