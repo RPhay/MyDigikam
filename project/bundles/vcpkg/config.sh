a0e0eb1a2f151b43964923882f76910cfb144812
Gilles Caulier <caulier.gilles@gmail.com>
2026-05-21 15:03:31 +0200

    bump version

--- project/bundles/vcpkg/config.sh
+++ project/bundles/vcpkg/config.sh
@@ -103,7 +103,7 @@ else
 
     # KDE KF6 frameworks version.
     # See official release here: https://download.kde.org/stable/frameworks/
-    DK_KDE_VERSION="v6.25.0"
+    DK_KDE_VERSION="v6.26.0"
 
     # KDE Plasma version.
     # See official release here: https://download.kde.org/stable/plasma/
@@ -111,7 +111,7 @@ else
 
     # KDE Application version.
     # See official release here: https://download.kde.org/stable/release-service/
-    DK_KA_VERSION="v25.12.3"
+    DK_KA_VERSION="v26.04.1"
 
     DK_UPLOADDIR="/srv/archives/files/digikam"
 
