4e649c7a5afe73f9a808f446d8c443f8c07bcc75
Gilles Caulier <caulier.gilles@gmail.com>
2026-05-23 18:29:13 +0200

    krazy++

--- core/cmake/modules/FindASan.cmake
+++ core/cmake/modules/FindASan.cmake
@@ -1,33 +1,16 @@
-#
-# The MIT License (MIT)
-#
-# Copyright (c) 2013 Matthew Arsenault
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in
-# all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-# THE SOFTWARE.
-#
+
 # This module tests if address sanitizer is supported by the compiler,
 # and creates a ASan build type (i.e. set CMAKE_BUILD_TYPE=ASan to use
 # it). This sets the following variables:
 #
-# CMAKE_C_FLAGS_ASAN - Flags to use for C with asan
-# CMAKE_CXX_FLAGS_ASAN  - Flags to use for C++ with asan
+# CMAKE_C_FLAGS_ASAN     - Flags to use for C with asan
+# CMAKE_CXX_FLAGS_ASAN   - Flags to use for C++ with asan
 # HAVE_ADDRESS_SANITIZER - True or false if the ASan build type is available
+#
+# SPDX-FileCopyrightText: 2013      by Matthew Arsenault
+#
+# SPDX-License-Identifier: MIT
+#
 
 include(CheckCXXCompilerFlag)
 
--- core/cmake/modules/FindUBSan.cmake
+++ core/cmake/modules/FindUBSan.cmake
@@ -1,26 +1,3 @@
-#
-# The MIT License (MIT)
-#
-# Copyright (c) 2013 Matthew Arsenault
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in
-# all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-# THE SOFTWARE.
-#
 
 # Check if the compiler supports a working ubsan. Provides a UBSan
 # build type, which is essentially Debug + ubsan. The flag can be used
@@ -31,9 +8,12 @@
 # HAVE_UNDEFINED_BEHAVIOR_SANITIZER - True or false if the UBSan is available
 # UNDEFINED_BEHAVIOR_SANITIZER_FLAG - Flag to add to compiler to use ubsan if supported
 #
-# CMAKE_C_FLAGS_UBSAN - Flags to use for C with ubsan
-# CMAKE_CXX_FLAGS_UBSAN  - Flags to use for C++ with ubsan
-##
+# CMAKE_C_FLAGS_UBSAN   - Flags to use for C with ubsan
+# CMAKE_CXX_FLAGS_UBSAN - Flags to use for C++ with ubsan
+#
+# SPDX-FileCopyrightText: 2013      by Matthew Arsenault
+#
+# SPDX-License-Identifier: MIT
 #
 
 include(CheckCXXCompilerFlag)
